35d0b88f4d884761da8a310ce30e2fe835b42aa2
Rossamylne <l.bader26@yahoo.com>
2023-07-22 21:37:22 +0200

    Do the rock paper scissors project

---
+++ script.js
@@ -0,0 +1,66 @@
+/*  
+    0 : Rock
+    1 : Paper
+    2 : Scissors
+*/
+
+
+function getComputerChoice() {
+    let choiceConversion = "";
+    let generatedChoice = Math.random();
+    switch (parseInt(generatedChoice * 3)) {
+        case 0:
+            choiceConversion = "rock";
+            break;
+        case 1:
+            choiceConversion = "paper"
+            break;
+        case 2:
+            choiceConversion = "scissors"
+            break;
+    }
+    return choiceConversion;
+}
+
+function round(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "scissors" 
+        || playerSelection == "paper" && computerSelection == "rock"
+        || playerSelection == "scissors" && computerSelection == "paper") {
+            return 2;
+        }
+    else if (playerSelection == computerSelection) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+
+function game() {
+    let playerScore, computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let inputedChoice = prompt("rock, paper or scissors ?");
+        const playerChoice = inputedChoice.toLowerCase();
+        const computerChoice = getComputerChoice();
+        console.log(`Player choice is ${playerChoice} and computer choice is ${computerChoice}`);
+        switch(round(playerChoice, computerChoice)) {
+        case 2:
+            console.log(`You win, ${playerChoice} beats ${computerChoice}`);
+            playerScore ++;
+            break;
+        case 1:
+            console.log("Equality !");
+            break;
+        case 0:
+            console.log(`You lose, ${computerChoice} beats ${playerChoice}`);
+            computerScore ++;
+            break;
+        }
+    (playerScore > computerScore) ? console.log(`You win, you have ${playerScore} points and computer has ${computerScore} points`) :
+    (playerScore == computerScore) ? console.log(`Equality !`):
+    console.log(`You lose, you have ${playerScore} points and computer has ${computerScore} points`)
+    } 
+}
+
+game();
